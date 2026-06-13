b2bf49216599e2ab6bb005065f8694fa9599441e
dlehdrlm <nicholausd@naver.com>
2026-06-13 13:47:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-NanumGothic.ttf
+from matplotlib import font_manager
+
+fonts = [f.fname for f in font_manager.fontManager.ttflist]
+
+korean_font = None
+
+for f in fonts:
+    if (
+        "Nanum" in f
+        or "Malgun" in f
+        or "NotoSansCJK" in f
+        or "Noto Sans CJK" in f
+    ):
+        korean_font = f
+        break
+
+st.write("찾은 폰트:", korean_font)
